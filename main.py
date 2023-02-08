c7d0410255167861ec3a502e7612f8911a871db9
sekkarin <73205970+sekkarin@users.noreply.github.com>
2023-02-08 11:20:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,80 @@
 import time
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.by import By
+
+
+
+# s = Service('C:\driver\msedgedriver.exe')
+# driver = webdriver.Edge(service=s)
+
+# driver.get("https://nxtgenaiacademy.com/demo-site/")
+
+# driver.find_element(By.ID, "vfb-5").send_keys('Sekkarin')
+# driver.find_element(By.ID, "vfb-7").send_keys('Singhayoo')
+
+# driver.find_element(By.XPATH, "//label[contains(.,'Male')]")
+# driver.find_element(By.ID, "vfb-13-address").send_keys('11/1')
+# driver.find_element(By.ID, "vfb-13-address-2").send_keys('123')
+# driver.find_element(By.ID, "vfb-13-city").send_keys('kennnn')
+# time.sleep(2)
+# driver.find_element(By.ID, "vfb-13-state").send_keys('nakornpathom')
+# driver.find_element(By.ID, "vfb-13-zip").send_keys('nakornpathom')
+# driver.find_element(By.XPATH, "//li[@id='item-vfb-13']/div/span[6]/span/span/span")
+# time.sleep(2)
+# driver.find_element(By.ID, "vfb-14").send_keys('644259021@webmail.npru.ac.th')
+# driver.find_element(By.XPATH, "//li[@id='item-vfb-16']/span/span/span/span/span[2]")
+# driver.find_element(By.ID, "vfb-19").send_keys('08000000000')
+# driver.find_element(By.XPATH, "//li[@id='item-vfb-20']/div/span/label")
+# time.sleep(2)
+# driver.find_element(By.ID, "vfb-23").send_keys('Hello wolrd')
+# driver.find_element(By.ID, "vfb-3").send_keys('55')
+# driver.find_element(By.ID, "vfb-4").click()
+# assert "Registration Form is Successfully Submitted." in driver.page_source
+
+# time.sleep(10)
+# driver.close()
+
+import unittest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("https://nxtgenaiacademy.com/demo-site/")
+
+        driver.find_element(By.ID, "vfb-5").send_keys('Sekkarin')
+        driver.find_element(By.ID, "vfb-7").send_keys('Singhayoo')
+
+        driver.find_element(By.XPATH, "//label[contains(.,'Male')]")
+        driver.find_element(By.ID, "vfb-13-address").send_keys('11/1')
+        driver.find_element(By.ID, "vfb-13-address-2").send_keys('123')
+        driver.find_element(By.ID, "vfb-13-city").send_keys('kennnn')
+        time.sleep(2)
+        driver.find_element(By.ID, "vfb-13-state").send_keys('nakornpathom')
+        driver.find_element(By.ID, "vfb-13-zip").send_keys('nakornpathom')
+        driver.find_element(By.XPATH, "//li[@id='item-vfb-13']/div/span[6]/span/span/span")
+        time.sleep(2)
+        driver.find_element(By.ID, "vfb-14").send_keys('644259021@webmail.npru.ac.th')
+        driver.find_element(By.XPATH, "//li[@id='item-vfb-16']/span/span/span/span/span[2]")
+        driver.find_element(By.ID, "vfb-19").send_keys('08000000000')
+        driver.find_element(By.XPATH, "//li[@id='item-vfb-20']/div/span/label")
+        time.sleep(2)
+        driver.find_element(By.ID, "vfb-23").send_keys('Hello wolrd')
+        driver.find_element(By.ID, "vfb-3").send_keys('55')
+        driver.find_element(By.ID, "vfb-4").click()
+        self.assertIn( "Registration Form is Successfully Submitted.", driver.page_source)
 
 
-s = Service('C:\driver\msedgedriver.exe')
-driver = webdriver.Edge(service=s)
+    def tearDown(self):
+        self.driver.close()
 
-driver.get("https://nxtgenaiacademy.com/demo-site/")
-
-driver.find_element(By.ID, "vfb-5").send_keys('Sekkarin')
-driver.find_element(By.ID, "vfb-7").send_keys('Singhayoo')
-
-driver.find_element(By.XPATH, "//label[contains(.,'Male')]")
-driver.find_element(By.ID, "vfb-13-address").send_keys('11/1')
-driver.find_element(By.ID, "vfb-13-address-2").send_keys('123')
-driver.find_element(By.ID, "vfb-13-city").send_keys('kennnn')
-driver.find_element(By.ID, "vfb-13-state").send_keys('nakornpathom')
-driver.find_element(By.ID, "vfb-13-zip").send_keys('nakornpathom')
-driver.find_element(By.XPATH, "//li[@id='item-vfb-13']/div/span[6]/span/span/span")
-driver.find_element(By.ID, "vfb-14").send_keys('644259021@webmail.npru.ac.th')
-driver.find_element(By.XPATH, "//li[@id='item-vfb-16']/span/span/span/span/span[2]")
-driver.find_element(By.ID, "vfb-19").send_keys('08000000000')
-driver.find_element(By.XPATH, "//li[@id='item-vfb-20']/div/span/label")
-driver.find_element(By.ID, "vfb-23").send_keys('Hello wolrd')
-driver.find_element(By.ID, "vfb-3").send_keys('55')
-driver.find_element(By.ID, "vfb-4").click()
-assert "Registration Form is Successfully Submitted." in driver.page_source
-
-time.sleep(10)
-driver.close()
+if __name__ == "__main__":
+    unittest.main()
